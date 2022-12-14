5d1ed684f1a46ec6876d02cdd61ec34b35af4fee
jithstephen13 <jithstephen96@gma.com>
2022-12-15 00:59:01 +0530

    add day 2

--- shop/src/pages/AllRoutes.jsx
+++ shop/src/pages/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import AdminPage from "./AdminPage";
  
 import Homepage from "./Homepage";
 
@@ -8,7 +9,8 @@ import Homepage from "./Homepage";
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+   
+      <Route path="/adminPage" element={<AdminPage />} />
      
       
     </Routes>
