619635d70bcce5a8fdcc67601b7893894a62990c
Shivam00002 <dubey02shivam@gmail.com>
2022-12-16 22:21:45 +0530

    sidebar

--- shop/src/pages/Clothes.jsx
+++ shop/src/pages/Clothes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import SimpleSidebar from "./Sidebar";
-import { Flex,Grid,Box ,Text, Button,Link} from "@chakra-ui/react";
+import Sidebar from "./Sidebar";
+import { Flex,Grid,Box ,Text, Button,Link,Image} from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect , useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {setItem} from '../utility/localStorage'
+import "../index.css"
 const Clothes = () => {
   const [data,setData]=useState([]);
+  const [loading ,setLoading]=useState(false);
   const navigate=useNavigate();
 
   const handleClick=(item)=>{
@@ -14,48 +16,62 @@ const Clothes = () => {
     navigate("/clothes/singleproduct")
  }
  useEffect(() => {
+  setLoading(true)
     axios.get("https://next-backend-orpin.vercel.app/cloths").then((response) => {
      console.log("res",response.data)
      setData(response.data)
+     setLoading(false)
     });
   }, []);
 
-  return (
-    <div>
-      <h1>Clothes</h1>
-      <Flex>
-      <SimpleSidebar/>
-     <Grid templateColumns='repeat(3, 1fr)' gap={6}>
-     
-       {data.map((el)=>{
-         return <Box  key={el.id}>
-          <Box textAlign={"left"}>
-          <img src={el.image1} alt="" />
-          <Text fontSize={17} >{el.title}</Text>
-          <Flex gap={2}>
-            <img  width={17} src="https://img.shop.com/Image/resources/images/onecart-icon.svg" alt="" />
-            <Text fontSize={13}>Sold by {el.soldby}</Text>
-
-          </Flex>
-          <p>{el.category}</p>
-          <Text fontWeight={"bold"}>{el.price}</Text>
-          <Text color={"teal"} fontSize={14}>Free shipping with $50.00 orders</Text>
-
-          <Button backgroundColor={"blue.300"} onClick={()=>handleClick(el)} borderRadius={25} width={85}>View</Button>
-
-       
-          </Box>
-          
-        </Box>
-      })}
   
-    </Grid>
-      </Flex>
+  {
+    if(loading){
+      return <Image marginLeft={635} src={"https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif?20170503175831"} fontSize={27} style={{"marginTop":"200px"}}></Image>
+    }else{
+    
+      return (
+        <div style={{"marginTop":"100px"}}>
       
+          <Flex>
+          <Sidebar/>
 
-     
-    </div>
-  );
+         <Grid marginLeft={"240px"} paddingLeft={"15px"} width={"80%"} templateColumns='repeat(3, 1fr)' gap={6}>
+         
+           {data.map((el)=>{
+             return <Box id='probox' key={el.id}>
+              <Box textAlign={"left"}>
+              <img id="hov" src={el.image1} alt="" />
+              <Text fontSize={17} >{el.title}</Text>
+              <Flex gap={2}>
+                <img  width={17} src="https://img.shop.com/Image/resources/images/onecart-icon.svg" alt="" />
+                <Text fontSize={13}>Sold by {el.soldby}</Text>
+    
+              </Flex>
+              <p>{el.category}</p>
+              <Text fontWeight={"bold"}>{el.price}</Text>
+              <Text color={"teal"} fontSize={14}>Free shipping with $50.00 orders</Text>
+    
+              <Button backgroundColor={"blue.300"} onClick={()=>handleClick(el)} marginTop={"-65px"} borderRadius={25} width={85} marginLeft={"70%"}>View</Button>
+    
+           
+              </Box>
+              
+            </Box>
+          })}
+      
+        </Grid>
+          </Flex>
+          
+    
+         
+        </div>
+      );
+    }
+  }
+
+
+ 
 };
 
 export default Clothes;
--- shop/src/pages/Sidebar.jsx
+++ shop/src/pages/Sidebar.jsx
@@ -1,161 +1,55 @@
-import React, { ReactNode } from 'react';
-import {
-  IconButton,
-  Box,
-  CloseButton,
-  Flex,
-  Icon,
-  useColorModeValue,
-  Link,
-  Drawer,
-  DrawerContent,
-  Text,
-  useDisclosure,
-  BoxProps,
-  FlexProps,
-} from '@chakra-ui/react';
-import {
-  FiHome,
-  FiTrendingUp,
-  FiCompass,
-  FiStar,
-  FiSettings,
-  FiMenu,
-} from 'react-icons/fi';
-import { IconType } from 'react-icons';
-import { ReactText } from 'react';
+import React from 'react'
+import { Flex,Grid,Box ,Text, Button,Link,Image, Heading} from "@chakra-ui/react";
+import "../index.css";
 
-interface LinkItemProps {
-  name: string;
-  icon: IconType;
-}
-const LinkItems: Array<LinkItemProps> = [
-
-   { name: 'Home', icon: FiHome },
-  { name: 'Trending', icon: FiTrendingUp },
-  { name: 'Explore', icon: FiCompass },
-  { name: 'Favourites', icon: FiStar },
-  { name: 'Settings', icon: FiSettings },
-];
-
-export default function SimpleSidebar({ children }: { children: ReactNode }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+const Sidebar = () => {
   return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-      <SidebarContent
-        onClose={() => onClose}
-        display={{ base: 'none', md: 'block' }}
-      />
-      <Drawer
-        autoFocus={false}
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        onOverlayClick={onClose}
-        size="full">
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
-        </DrawerContent>
-      </Drawer>
-      {/* mobilenav */}
-      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="4">
-        {children}
-      </Box>
-    </Box>
-  );
-}
+    <>
+ <Box id='maindiv' border={"1px solid"} width={"20%"}>
+  <Text marginTop={2} color={"teal"} id="clothe" fontSize={35} fontWeight={"bold"}>CLOTHES</Text>
+  <Box marginLeft={"40px"}   textAlign={"left"} marginTop={"15px"}>
+<Flex alignItems={"center"} gap={"15px"}> 
+  <Image marginTop={"8px"} borderRadius={"50%"} height={"25px"} width={"25px"} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaXelByMSTcBlhsGChcrAWlXVXNXxI53LxzirHbHwGJQ&s'></Image>
+  <Text className='menu'  fontWeight={"bold"}>Mens</Text></Flex>
 
-interface SidebarProps extends BoxProps {
-  onClose: () => void;
-}
-
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
-  return (
-    <Box
-      bg={useColorModeValue('white', 'gray.900')}
-      borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}>
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Clothes
-        </Text>
-        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
-      </Flex>
-      {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
-      ))}
-    </Box>
-  );
-};
-
-interface NavItemProps extends FlexProps {
-  icon: IconType;
-  children: ReactText;
-}
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
-  return (
-    <Link href="#" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: 'cyan.400',
-          color: 'white',
-        }}
-        {...rest}>
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: 'white',
-            }}
-            as={icon}
-          />
-        )}
-        {children}
-      </Flex>
-    </Link>
-  );
-};
-
-interface MobileProps extends FlexProps {
-  onOpen: () => void;
-}
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 24 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue('white', 'gray.900')}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent="flex-start"
-      {...rest}>
-      <IconButton
-        variant="outline"
-        onClick={onOpen}
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-
-      <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
+  <Flex alignItems={"center"} gap={"15px"}> 
+  <Image marginTop={"8px"} borderRadius={"50%"} height={"25px"} width={"25px"} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROZH_L731sgBrBunH8f5lp6pLAhvehf2DuZJGjFeI&s'></Image>
+  <Text className='menu'  fontWeight={"bold"}>Womes</Text></Flex>
+   
     
-      </Text>
-    </Flex>
-  );
-};
+  <Flex alignItems={"center"} gap={"15px"}> 
+  <Image marginTop={"8px"} borderRadius={"50%"} height={"25px"} width={"25px"} src='https://img.icons8.com/color/2x/children.png'></Image>
+  <Text className='menu'  fontWeight={"bold"}>Kids</Text></Flex>
+
+
+  </Box>
+
+  <Box id="filter">
+  <select name="" id="">
+     <option value="reset">Filter by Price</option>
+      <option value="high">Low to high </option>
+      <option value="low">High to low</option>
+     </select>
+  </Box>
+ 
+ <Box id="add" marginLeft={"0px"} textAlign={"left"} marginTop={"50px"}>
+  <img src="https://img.shop.com/Image/topbrands/nmlogos_76181.gif" alt="" />
+  <Text> Up to 6.00% Cashback <br/>
+Store conditions </Text>
+<Button>partner Site 🤝</Button>
+ </Box>
+
+ <Box id="add" marginLeft={"0px"} textAlign={"left"} marginTop={"20px"}>
+  <img src="https://img.shop.com/Image/topbrands/nmlogos_98199.gif" alt="" />
+  <Text> Up to 6.00% Cashback <br/>
+Store conditions </Text>
+<Button>partner Site 🤝</Button>
+ </Box>
+  </Box>
+
+
+  </>
+  )
+}
+
+export default Sidebar
