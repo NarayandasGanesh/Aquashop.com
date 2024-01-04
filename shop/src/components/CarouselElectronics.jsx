afa8e8082810761fd140051fa7430f92c9110fff
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-01-04 23:18:42 +0530

    Update CarouselElectronics.jsx

--- shop/src/components/CarouselElectronics.jsx
+++ shop/src/components/CarouselElectronics.jsx
@@ -33,7 +33,6 @@ const settings = {
   initialSlide: 0,
 };
 
-
 export default function CarouselElectronics() {
   // As we have used custom buttons, we need a reference variable to
   // change the state
